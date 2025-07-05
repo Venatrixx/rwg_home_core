a3af1d248f0a0d986173da069b149331e354650a
Venatrix <venatrix945@gmail.com>
2025-07-05 15:15:23 +0200

    changed Feiertag to Ferientag for holiday events

--- lib/src/calendar/event.dart
+++ lib/src/calendar/event.dart
@@ -14,7 +14,7 @@ class Event {
   String? time;
 
   Event(this.date, this.type, this.title, {this.time, this.comment, this.triState});
-  Event.holiday(this.date, {this.time, this.comment, this.triState}) : type = EventType.holiday, title = "Feiertag";
+  Event.holiday(this.date, {this.time, this.comment, this.triState}) : type = EventType.holiday, title = "Ferientag";
   Event.missingDay(this.date, this.title, {this.time, this.comment, this.triState}) : type = EventType.missingDay;
 
   Event.eventFromJson(dynamic json) {
