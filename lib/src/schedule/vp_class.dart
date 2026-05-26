257935d53796c21ee2331a33925fe1b6512814e8
Venatrix <venatrix945@gmail.com>
2026-05-26 15:16:32 +0200

    feature: added getGroupedSubjects method

--- lib/src/schedule/vp_class.dart
+++ lib/src/schedule/vp_class.dart
@@ -8,27 +8,65 @@ class VPClass {
   late List<VPSubject> subjects;
   late List<VPLesson> lessons;
 
-  VPClass({required this.name, this.times = const [], required this.subjects, required this.lessons});
+  VPClass({
+    required this.name,
+    this.times = const [],
+    required this.subjects,
+    required this.lessons,
+  });
 
   VPClass.fromXML(XmlElement xmlObject) {
     name = xmlObject.findAllElements('Kurz').first.text;
 
-    times = [for (final time in xmlObject.findAllElements('KlSt')) VPTime.fromXML(time)];
+    times = [
+      for (final time in xmlObject.findAllElements('KlSt'))
+        VPTime.fromXML(time),
+    ];
 
-    subjects = [for (final subject in xmlObject.findAllElements('Ue')) VPSubject.fromXML(subject)];
+    subjects = [
+      for (final subject in xmlObject.findAllElements('Ue'))
+        VPSubject.fromXML(subject),
+    ];
 
-    lessons = [for (final lesson in xmlObject.findAllElements('Std')) VPLesson.fromXML(lesson)];
+    lessons = [
+      for (final lesson in xmlObject.findAllElements('Std'))
+        VPLesson.fromXML(lesson),
+    ];
   }
 
   VPClass.fromJson(dynamic json)
     : name = json['name'].toString(),
       times = [],
-      subjects = [for (final entry in json['subjects'] ?? []) VPSubject.fromJson(entry)],
-      lessons = [for (final entry in json['lessons'] ?? []) VPLesson.fromJson(entry)];
+      subjects = [
+        for (final entry in json['subjects'] ?? []) VPSubject.fromJson(entry),
+      ],
+      lessons = [
+        for (final entry in json['lessons'] ?? []) VPLesson.fromJson(entry),
+      ];
 
   dynamic toJson() => {
     'name': name,
     'subjects': [for (final subject in subjects) subject.toJson()],
     'lessons': [for (final lesson in lessons) lesson.toJson()],
   };
+
+  Map<String, List<VPSubject>> getGroupedSubjects() {
+    Map<String, List<VPSubject>> result = {};
+
+    for (final subject in subjects) {
+      final name = subject.label.toLowerCase().replaceAll(RegExp('[0-9]'), '');
+
+      if (result.keys.contains(name)) {
+        result[name]!.add(subject);
+      } else {
+        result[name] = [subject];
+      }
+    }
+
+    for (var key in result.keys) {
+      key = "${key[0].toUpperCase()}${key.substring(1)}";
+    }
+
+    return result;
+  }
 }
